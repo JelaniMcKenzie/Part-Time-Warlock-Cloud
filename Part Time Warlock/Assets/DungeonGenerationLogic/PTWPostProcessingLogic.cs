d61de9edcbbf8671e86bda748abd8bda0f09925d
JelaniMcKenzie <jelani.p.mckenzie@gmail.com>
2024-02-15 22:33:21 -0500

    PTW room detection code creation
    Started implementing room detection for the game

--- Part Time Warlock/Assets/DungeonGenerationLogic/PTWPostProcessingLogic.cs
+++ Part Time Warlock/Assets/DungeonGenerationLogic/PTWPostProcessingLogic.cs
@@ -1,10 +1,12 @@
 using Edgar.Unity;
+using Edgar.Unity.Examples.CurrentRoomDetection;
 using Edgar.Unity.Examples.Gungeon;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 using static UnityEngine.EventSystems.EventTrigger;
 
 /// <summary>
@@ -25,6 +27,21 @@ public class PTWDungeonPostProcessingLogic : DungeonGeneratorPostProcessingCompo
 
         var walls = level.GetSharedTilemaps().First(x => x.name == "Walls");
         walls.gameObject.tag = "Border";
+
+        foreach (var roomInstance in level.RoomInstances)
+        {
+            var roomTemplateInstance = roomInstance.RoomTemplateInstance;
+
+            // Find floor tilemap layer
+            var tilemaps = RoomTemplateUtilsGrid2D.GetTilemaps(roomTemplateInstance);
+            var floor = tilemaps.Single(x => x.name == "Floor").gameObject;
+
+            // Add floor collider
+            AddFloorCollider(floor);
+
+            // Add current room detection handler
+            floor.AddComponent<CurrentRoomDetectionTriggerHandler>();
+        }
     }
 
     private void HandleEnemies(DungeonGeneratorLevelGrid2D level)
@@ -122,6 +139,19 @@ public class PTWDungeonPostProcessingLogic : DungeonGeneratorPostProcessingCompo
 
             }
         }
+    }
+
+    private void AddFloorCollider(GameObject floor)
+    {
+        var tilemapCollider2D = floor.AddComponent<TilemapCollider2D>();
+        tilemapCollider2D.usedByComposite = true;
+
+        var compositeCollider2d = floor.AddComponent<CompositeCollider2D>();
+        compositeCollider2d.geometryType = CompositeCollider2D.GeometryType.Polygons;
+        compositeCollider2d.isTrigger = true;
+        compositeCollider2d.generationType = CompositeCollider2D.GenerationType.Manual;
+
+        floor.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
     }
 
 }
